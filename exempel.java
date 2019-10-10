2bd14c901fb4deca17f0322284b0cc756d25bdc9
SimonVestman <simonw202@gmail.com>
2019-10-10 11:15:44 +0200

    reeeee

---
+++ exempel.java
@@ -0,0 +1,24 @@
+/*
+(for loop)
+public class exempel{
+	public static void main(String[] args) {
+		for (int i = 0 ; i < 14 ; i++) {
+			System.out.println("Hellow World!");
+		}
+	}
+}
+*/
+
+/*
+(while loop)
+public class exempel{
+	public static void main(String[] args) {
+		int i = 0;
+		while (i < 14){
+				System.out.println("Hello World!");
+				i ++;
+		}
+	}
+}
+*/
+
